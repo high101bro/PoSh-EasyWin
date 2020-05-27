30593044ffe7b77391ffc65012ac6b93a1fde716
high101bro <daniel.komnick@gmail.com>
2020-05-27 13:27:34 -0700

    View Change Log

--- PoSh-ACME_(Rebranding to PoSh-EasyWin)/Dependencies/Query Commands and Scripts/Scripts - Endpoint/Get-USBHistoryFromRegistry.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-ItemProperty -Path HKLM:\system\CurrentControlSet\Enum\USBSTOR\*\* | Select-Object -Property FriendlyName, Service, @{Name="ContainerID";Expression={"$($_.ContainerID.trim('{}'))"}}, @{Name="HardwareID";Expression={"$($_.HardwareID)"}}
-
-
-
-
