b02648e19dee02af439a1175cdefca1ba4c0cb29
Dan Komnick <daniel.komnick@gmail.com>
2021-09-17 16:02:04 -0400

    Update Start-FormTicker.ps1

--- Dependencies/Code/Main Body/Start-FormTicker.ps1
+++ Dependencies/Code/Main Body/Start-FormTicker.ps1
@@ -7,8 +7,8 @@ $script:PoShEasyWinFormTicker = New-Object System.Windows.Forms.Timer -Property 
 $script:PoShEasyWinFormTicker.add_Tick({
     $script:PoShEasyWinStatusBar.Text = "$(Get-Date) - Computers Selected [$($script:ComputerList.Count)], Queries Selected [$($script:SectionQueryCount)]"
 
-    $PoShEasyWinPSSessions = Get-PSSession
-    $ResultsListBox.Items.Insert(0,$PoShEasyWinPSSessions)
+    #$PoShEasyWinPSSessions = Get-PSSession
+    #$ResultsListBox.Items.Insert(0,$PoShEasyWinPSSessions)
     # Not working, the monitor-jobs script needs to be updated first
     #if ($ResultsFolderAutoTimestampCheckbox.checked) {
     #    $script:CollectionSavedDirectoryTextBox.Text = "$CollectedDataDirectory\$((Get-Date).ToString('yyyy-MM-dd HH.mm.ss'))"
